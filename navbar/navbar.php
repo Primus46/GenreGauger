3e0be20f6b63c1cb4e3593ccc675125fa63fe7b4
Primus46 <connord07@outlook.com>
2024-03-31 20:16:33 +1100

    added home page & navbar

--- navbar/navbar.php
+++ navbar/navbar.php
@@ -3,19 +3,12 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="HomePage.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">  
+  <link rel="stylesheet" href="navbar.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
     <body>
-        <?php include 'navbar/navbar.php';?>
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col">
-                    <div class="box"></div>
-                </div>
-                <div class="col">
-                    <div class="box"></div>
-                </div>
                 <div class="col">
                     <div class="box"></div>
                 </div>
