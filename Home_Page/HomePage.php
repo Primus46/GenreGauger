d44b775359bd489f876e17f27e5f51046a8ab8bd
Primus46 <connord07@outlook.com>
2024-03-31 21:25:35 +1100

    update

--- Home_Page/HomePage.php
+++ Home_Page/HomePage.php
@@ -7,7 +7,9 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">  
 </head>
     <body>
-        <?php include 'navbar/navbar.php';?>
+        <div class="container mt-3">
+            <img class="img-fluid" src="/images/wepik-simple-futuristic-dark-neon-gaming-twitter-header-20240308074847WF8g.png" alt="GenreGauger" width="1440" style="364"> 
+        </div>
         <div class="container">
             <div class="row">
                 <div class="col">
--- navbar/navbar.php
+++ navbar/navbar.php
@@ -5,15 +5,40 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="navbar.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
     <body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col">
-                    <div class="box"></div>
-                </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mynavbar">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                <a class="nav-link" href=""><h3>GenreGauger</h3></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href=""><h5>Reviews</h5></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href=""><h5>Genres</h5></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href=""><h5>Leaderboard</h5></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href=""><h5>Favorites</h5></a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="text" placeholder="Search">
+                <button class="btn btn-primary" type="button"><i class="fa fa-search"></i></button>
+            </form>
+            <button type="button" class="btn btn-outline-primary">Sign Up</button>
             </div>
         </div>
+    </nav>
     </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </html>
